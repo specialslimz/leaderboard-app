7a72e0313e0ee149e296cb5b4fd4b226a6770589
speciaslimz <specialslimz1842@1842.com>
2024-03-24 21:00:31 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import data from "./data.js";
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState(data);
+  const uniqueButtons = [...new Set(data.map((item) => item.path))];
+
+  function handleClick(id) {
+    const filterItems = data.filter((item) => item.path === id);
+    setItems(filterItems);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Button
+        uniqueButtons={uniqueButtons}
+        handleClick={handleClick}
+        handleAll={setItems}
+      />
+      <Board items={items} />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1 className="logo"> LEADERBOARD</h1>;
+}
+
+function Board({ items }) {
+  const [sortBy, setSortBy] = useState("all");
+  let sortedItems;
+  if (sortBy === "all") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+
+  if (sortBy === "highscore")
+    sortedItems = items
+      .slice()
+      .sort(
+        (a, b) => b.firstScore + b.secondScore - (a.firstScore + a.secondScore)
+      );
+  return (
+    <>
+      <div>
+        <ul className="board-list">
+          {sortedItems.map((score) => (
+            <List scores={score} key={score.id} />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="all">All</option>
+          <option value="description">Sort by name</option>
+          <option value="highscore">Sort by highscore</option>
+        </select>
+      </div>
+    </>
+  );
+}
+
+function List({ scores }) {
+  const total = scores.firstScore + scores.secondScore;
+
+  return (
+    <div className="list">
+      <figure>
+        <img src={scores.image} alt={scores.name} />
+      </figure>
+      <article className="list-details">
+        <header>
+          <h1> {scores.name}</h1>
+        </header>
+        <h3> Path: {scores.path}</h3>
+        <p> Task-1: {scores.firstScore}</p>
+        <p> Task-2: {scores.secondScore}</p>
+        <h3> TOTAL: {total}</h3>
+      </article>
+    </div>
+  );
+}
+
+const Button = ({ uniqueButtons, handleClick, handleAll }) => {
+  const [isActive, setIsActive] = useState(null);
+  return (
+    <div className="buttons">
+      {uniqueButtons.map((buttonTitle, index) => (
+        <button
+          className={` ${isActive === buttonTitle ? "active" : ""}`}
+          key={index}
+          onClick={() => {
+            handleClick(buttonTitle);
+            setIsActive(buttonTitle);
+          }}
+        >
+          {buttonTitle}
+        </button>
+      ))}
+      <button
+        key="all"
+        onClick={() => {
+          handleAll(data);
+          setIsActive("all");
+        }}
+        className={isActive === "all" || isActive === null ? "active" : ""}
+      >
+        All
+      </button>
+    </div>
+  );
+};
---
+++ src/data.js
@@ -0,0 +1,221 @@
+const data = [
+  {
+    id: 1,
+    name: "John",
+    path: "frontend",
+    firstScore: 5,
+    secondScore: 10,
+    image:
+      "https://i.pinimg.com/564x/90/66/20/906620feb2f7f9bb6a0dca41f70f5eee.jpg",
+  },
+  {
+    id: 2,
+    name: "peace",
+    path: "frontend",
+    firstScore: 10,
+    secondScore: 8,
+    image:
+      "https://i.pinimg.com/564x/3d/98/dd/3d98ddb5a4292888ab8ec371df03b593.jpg",
+  },
+  {
+    id: 3,
+    name: "caleb",
+    path: "frontend",
+    firstScore: 6,
+    secondScore: 6,
+    image:
+      "https://i.pinimg.com/564x/28/0a/40/280a4088e16555567f3a04f6e29e7b7c.jpg",
+  },
+
+  {
+    id: 4,
+    name: "josphine",
+    path: "frontend",
+    firstScore: 4,
+    secondScore: 8,
+    image:
+      "https://i.pinimg.com/564x/da/c4/19/dac4198b0add1d98d7d32523f8a79ee7.jpg",
+  },
+  {
+    id: 5,
+    name: "chisom",
+    path: "frontend",
+    firstScore: 9,
+    secondScore: 1,
+    image:
+      "https://i.pinimg.com/474x/28/6c/76/286c760f394af0190adb44a1134c0790.jpg",
+  },
+  {
+    id: 6,
+    name: "david",
+    path: "frontend",
+    firstScore: 7,
+    secondScore: 3,
+    image:
+      "https://i.pinimg.com/564x/94/d9/35/94d935055a035dea7d32cc10ff14874b.jpg",
+  },
+  {
+    id: 7,
+    name: "chioma",
+    path: "backend",
+    firstScore: 10,
+    secondScore: 3,
+    image:
+      "https://i.pinimg.com/564x/69/84/ac/6984ac77c63f747a35d508cdaf531c9b.jpg",
+  },
+  {
+    id: 8,
+    name: "solomon",
+    path: "backend",
+    firstScore: 9,
+    secondScore: 3,
+    image:
+      "https://i.pinimg.com/564x/28/0a/40/280a4088e16555567f3a04f6e29e7b7c.jpg",
+  },
+  {
+    id: 9,
+    name: "daniel",
+    path: "backend",
+    firstScore: 1,
+    secondScore: 3,
+    image:
+      "https://i.pinimg.com/564x/9b/81/87/9b818777caeaa0f66a52a8adf2263c09.jpg",
+  },
+  {
+    id: 10,
+    name: "ada",
+    path: "backend",
+    firstScore: 5,
+    secondScore: 3,
+    image:
+      "https://i.pinimg.com/564x/a4/43/79/a443799bbec55b333874a16c751bda1a.jpg",
+  },
+  {
+    id: 11,
+    name: "favour",
+    path: "backend",
+    firstScore: 10,
+    secondScore: 3,
+    image:
+      "https://i.pinimg.com/564x/76/ed/3d/76ed3d8963f3f1ae68affac4ee8b8896.jpg",
+  },
+  {
+    id: 12,
+    name: "izuchukwu",
+    path: "backend",
+    firstScore: 10,
+    secondScore: 6,
+    image:
+      "https://i.pinimg.com/564x/6b/d6/f0/6bd6f0a5b32398cb90aa6dfa849a7033.jpg",
+  },
+  {
+    id: 13,
+    name: "victor",
+    path: "product design",
+    firstScore: 3,
+    secondScore: 2,
+    image:
+      "https://i.pinimg.com/564x/e8/9e/43/e89e439f50dd6ba6f9ade0c4d4646355.jpg",
+  },
+  {
+    id: 14,
+    name: "michael",
+    path: "product design",
+    firstScore: 9,
+    secondScore: 7,
+    image:
+      "https://i.pinimg.com/564x/11/8d/6c/118d6c077998adafee3ad7bba371a5b0.jpg",
+  },
+  {
+    id: 15,
+    name: "chijioke",
+    path: "product design",
+    firstScore: 7,
+    secondScore: 7,
+    image:
+      "https://i.pinimg.com/564x/0f/31/25/0f3125694022b5d96d4d0d181dcd22b3.jpg",
+  },
+  {
+    id: 16,
+    name: "ebube",
+    path: "product design",
+    firstScore: 7,
+    secondScore: 10,
+    image:
+      "https://i.pinimg.com/564x/e3/17/d9/e317d9a6aec615c45dcf504af07b2b76.jpg",
+  },
+  {
+    id: 17,
+    name: "ebuka",
+    path: "product design",
+    firstScore: 7,
+    secondScore: 1,
+    image:
+      "https://i.pinimg.com/564x/5a/8c/e7/5a8ce76174bf27ebcb92503fd2e43b58.jpg",
+  },
+  {
+    id: 18,
+    name: "sonia",
+    path: "product design",
+    firstScore: 7,
+    secondScore: 2,
+    image:
+      "https://i.pinimg.com/564x/f0/5a/63/f05a63add8138ba1d28832db0ad0ce6c.jpg",
+  },
+  {
+    id: 19,
+    name: "idam",
+    path: "web-3",
+    firstScore: 7,
+    secondScore: 9,
+    image:
+      "https://i.pinimg.com/564x/ce/9d/87/ce9d87ba98a649faa375cb7ffd188ad3.jpg",
+  },
+  {
+    id: 20,
+    name: "judith",
+    path: "web-3",
+    firstScore: 3,
+    secondScore: 9,
+    image:
+      "https://i.pinimg.com/564x/a0/51/9a/a0519ac1ad98a238e9fbf543e4e716c8.jpg",
+  },
+  {
+    id: 21,
+    name: "stella",
+    path: "web-3",
+    firstScore: 5,
+    secondScore: 5,
+    image:
+      "https://i.pinimg.com/564x/2c/45/bf/2c45bff30470d76c72702683574d1f47.jpg",
+  },
+  {
+    id: 22,
+    name: "munachi",
+    path: "web-3",
+    firstScore: 0,
+    secondScore: 5,
+    image:
+      "https://i.pinimg.com/564x/76/b6/ac/76b6acfd49fe672ce39b85b47dc0669a.jpg",
+  },
+  {
+    id: 23,
+    name: "chibuzor",
+    path: "web-3",
+    firstScore: 10,
+    secondScore: 5,
+    image:
+      "https://i.pinimg.com/564x/86/10/c1/8610c1e961ca2a1c4b231dd8f30fe707.jpg",
+  },
+  {
+    id: 24,
+    name: "victory",
+    path: "web-3",
+    firstScore: 10,
+    secondScore: 10,
+    image:
+      "https://i.pinimg.com/564x/6d/b2/91/6db2913b3352dc36defd69b7ca56e199.jpg",
+  },
+];
+
+export default data;
